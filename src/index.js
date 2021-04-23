9a7c83b9b4323dbbaa5b50abc21d405d357d1333
afaafa <afhazachra@gmail.com>
2021-04-23 23:48:47 +0700

    add component header and footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './Main';
+import App from './App';
 import './style.css';
 
 ReactDOM.render(
-    <Main/>,
+    <App/>,
   document.getElementById('root')
 );
